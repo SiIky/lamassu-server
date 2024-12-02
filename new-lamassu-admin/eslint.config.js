dbf91f7b6cc8e5c478a6cbd98e0ae8b945b8a4ad
Rafael <rafael.taranto@protonmail.com>
2024-12-02 10:38:22 +0000

    chore: react-compiler eslint

--- new-lamassu-admin/eslint.config.js
+++ new-lamassu-admin/eslint.config.js
@@ -1,11 +1,12 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
+import reactCompiler from 'eslint-plugin-react-compiler'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
@@ -16,17 +17,21 @@ export default [
     settings: {
       react: {
         version: '16'
-      }
+      },
+    },
+    plugins: {
+      'react-compiler': reactCompiler
     }
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
+      'no-unused-vars': 'off',
       'react/prop-types': 'off',
       'react/display-name': 'off',
       'react/no-unescaped-entities': 'off',
-      'no-unused-vars': 'off',
+      'react-compiler/react-compiler': 'warn',
     }
   }
 ]
